1eabb4610d215f34f9469816d02e9f4b9ac3f87f
Agustin <agustin.zalazar@uai.edu.ar>
2020-08-03 15:16:26 -0300

    agregar docs

---
+++ docs/precache-manifest.6986a9aca4bb7268649e4d5561164827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454fd5cea879acc8e3e104564b92066a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e386dc5e0195cff40bb8",
+    "url": "/static/css/main.820b2127.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e386dc5e0195cff40bb8",
+    "url": "/static/js/main.8490f664.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
